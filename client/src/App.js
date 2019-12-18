ee04c6d1e8b3e73435b2c89da270e35af0d53360
Jason Long <jlong5795@gmail.com>
2019-12-18 17:16:22 -0600

    Stuck, cannot achieve MVP

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <Route exact path='/'>
         <MovieList />
       </Route>
-      <Route path={`/movies/:${movie.id}`}>
+      <Route path='/movies/:movieID'>
         <Movie />
       </Route>
     </div>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, useRouteMatch } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const [id, setID] = useState();
+   
   useEffect(() => {
-    const id = 1;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
-          console.log(response);
+          console.log("response", response.data);
           setMovie(response.data);
         })
         .catch(error => {
